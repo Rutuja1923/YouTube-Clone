454e24f176e64b3c657f3393b8c873e96e15656b
Rutuja1923 <rutujajirage19@gmail.com>
2024-10-09 22:51:10 +0530

    adding script for toggling the menu icon

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuIcon = document.querySelector("#hamburger");
 const sideBar = document.querySelector(".side-bar")
-
+const container = document.querySelector(".main-container")
 menuIcon.onclick=function(){
     sideBar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
 }
